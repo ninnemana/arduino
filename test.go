c4a9fb6219139c8815738f8d6f420964425a7faa
Alex Ninneman <alex@ninneman.org>
2016-04-06 21:27:38 -0500

    Initial commit

---
+++ test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"time"
+
+	"github.com/hybridgroup/gobot"
+	"github.com/hybridgroup/gobot/platforms/firmata"
+	"github.com/hybridgroup/gobot/platforms/gpio"
+)
+
+func main() {
+	gbot := gobot.NewGobot()
+	gbot.AutoStop = true
+
+	firmataAdaptor := firmata.NewFirmataAdaptor("arduino", "/dev/tty.usbmodem1411")
+	led := gpio.NewLedDriver(firmataAdaptor, "led", "13")
+
+	work := func() {
+		gobot.Every(1*time.Second, func() {
+			led.Toggle()
+		})
+	}
+
+	robot := gobot.NewRobot("bot",
+		[]gobot.Connection{firmataAdaptor},
+		[]gobot.Device{led},
+		work,
+	)
+
+	gbot.AddRobot(robot)
+
+	gbot.Start()
+
+}
